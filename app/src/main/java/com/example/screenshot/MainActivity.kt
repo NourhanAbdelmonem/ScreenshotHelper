36fe13e1761126b988c698672db2fb9b246d3b3a
hossam <habdelnaby@innovationteam.com>
2025-01-16 10:32:23 +0200

    test

--- app/src/main/java/com/example/screenshot/MainActivity.kt
+++ app/src/main/java/com/example/screenshot/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.example.screenshot
 
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+class MainActivity : AppCompatActivity(), Activity.ScreenCaptureCallback {
+
+    lateinit var data: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
         val screenshotHelper = ScreenshotHelper(this)
-        val data: TextView = findViewById(R.id.data)
+        data = findViewById(R.id.data)
         val takeScreenshotButton: Button = findViewById(R.id.takeScreenshotButton)
 
         takeScreenshotButton.setOnClickListener {
@@ -30,6 +42,37 @@ class MainActivity : AppCompatActivity() {
                 ).show()
 
             }
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            registerScreenCaptureCallback(mainExecutor, this)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            unregisterScreenCaptureCallback(this)
+        }
+    }
+
+    override fun onScreenCaptured() {
+        val screenshotHelper = ScreenshotHelper(this)
+
+        lifecycleScope.launch {
+            if (screenshotHelper.takeScreenshotSuccessfully(listOf(data))) Toast.makeText(
+                this@MainActivity,
+                "Screenshot saved!",
+                Toast.LENGTH_SHORT
+            ).show()
+            else Toast.makeText(
+                this@MainActivity, "Failed to save screenshot.", Toast.LENGTH_SHORT
+            ).show()
+
         }
     }
 }
